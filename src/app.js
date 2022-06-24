3f9599b999f2b628c7e1c58dfc1717167b23bfb0
Imran Azam <imranazam3@gmail.com>
2022-06-24 22:57:09 +0100

    use json

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 var express = require('express')
 var app = express()
-var PORT = process.env.PORT || 8080
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.json({msg: "hello there!"})
 })
 
+const PORT = process.env.PORT || 8080
 app.listen(PORT, function () {
   console.log('App listening on port: ', PORT)
 })
